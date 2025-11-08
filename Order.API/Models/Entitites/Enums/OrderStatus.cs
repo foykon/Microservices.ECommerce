f41f0dab73a24c2dc61848b13a1d507e6cb36958
foykon <foykon9@gmail.com>
2025-11-08 16:22:06 +0300

    Add project files.

---
+++ Order.API/Models/Entitites/Enums/OrderStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.API.Models.Entitites.Enums
+{
+    public enum OrderStatus
+    {
+        Completed = 0,
+        Failed = 1,
+        Suspend = 2
+    }
+}
---
+++ Order.API/Models/OrderAPIDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.API.Models.Entitites;
+
+namespace Order.API.Models
+{
+    public class OrderAPIDbContext : DbContext
+    {
+        public OrderAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Entitites.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        
+
+    }
+}
